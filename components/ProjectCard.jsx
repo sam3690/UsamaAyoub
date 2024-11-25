144151a200eab1080ad945180b7169413689a280
sam3690 <usamabinayoub@gmail.com>
2024-11-25 13:21:46 +0500

    update 25-11-2024 1:21 pm

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -5,7 +5,7 @@ const ProjectCard = ({id, title, img, link}) => {
   return (
     <div key={id} className='justify-center sm:ml-8 sm:mx-0 w-auto min-w-[300px] h-[400px] '>
         <img src={img} alt={id} className='w-[100%] h-[300px] rounded-md object-cover object-top duration-1000 ease-in-out hover:object-bottom' />
-        <Link className='font-Poppins font-semibold text-lg leading-[32.4px] text-white my-10' href={link}>{title}</Link>
+        <Link className='font-Poppins font-semibold text-lg leading-[32.4px] text-white my-10' href={link} target='_blank'>{title}</Link>
         </div>
   )
 }
